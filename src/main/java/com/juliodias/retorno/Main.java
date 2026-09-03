7a9f040597a170bc0073d2142303f8fed3cac9bc
Julio Cezar <peixelahma@yahoo.com.br>
2026-09-02 21:46:42 -0300

    feat: atualiza a mensagem

--- src/main/java/com/juliodias/retorno/Main.java
+++ src/main/java/com/juliodias/retorno/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Produto produto = new Produto("Teclado", 250.00);
+        Produto produto = new Produto("Teclado:", 250.00);
 
         System.out.println(produto.getNome());
         System.out.println(produto.getPreco());
